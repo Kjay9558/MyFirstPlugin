6b5f1fd098b74d233b4966c8866ff7bbd1f18d21
Lisa Zimmermann <li.zimmermann@ostfalia.de>
2022-11-28 22:38:46 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BepInEx;
+using TinyResort;
+using UnityEngine;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace MyFirstPlugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("dev.TinyResort.TRTools")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static TRPlugin plugin;
+        
+        // Build an input simulator instance
+        InputSimulator inSim = new InputSimulator();
+        
+        private string hotkey1 = "P";
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            plugin = TRTools.Initialize(this, -1 /* TODO: add yourNexusID*/, "kjay");
+
+            TRTools.sceneSetupEvent += CreateHotkeyButtons;
+            plugin.Log("Created Objects: HotkeyButtons");
+        }
+
+        private void CreateHotkeyButtons()
+        {
+            GameHud = GameObject.Find("MapCanvas/Hud");
+            var buttonGrid = new GameObject();
+            buttonGrid.name = "Hotkey Buttons Grid";
+            try
+            {
+                buttonGrid.transform.SetParent(GameHud.transform);
+            }
+            catch
+            {
+                return;
+            }
+
+            var rect = buttonGrid.GetComponent<RectTransform>();
+            rect.pivot = new Vector2(0.5f, 1);
+            
+
+            HotkeyButton1 = TRInterface.CreateButton(ButtonTypes.MainMenu, buttonGrid.transform, "Hotkey1", useHotkey1);
+            HotkeyButton1.textMesh.GetComponent<RectTransform>().sizeDelta = new Vector2(50, 10);
+            HotkeyButton1.textMesh.fontSize = 8;
+
+        }
+
+        public TRButton HotkeyButton1 { get; set; }
+
+        public GameObject GameHud { get; set; }
+
+        private void useHotkey1()
+        {
+            inSim.Keyboard.KeyPress(VirtualKeyCode.VK_P);
+            plugin.Log("KeyPress Simulated: P");
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace MyFirstPlugin
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "kjay9558.dinkum.MyFirstPlugin";
+        public const string PLUGIN_NAME = "kjay9558.dinkum.MyFirstPlugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
